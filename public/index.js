0d8ac9afb66b86891530fdb80fbf086db48bbc26
hoodhahmed <96528096+hoodhahmed@users.noreply.github.com>
2023-10-25 16:45:29 +0000

    added pubg response

--- public/index.js
+++ public/index.js
@@ -1,30 +1,34 @@
 // Requiring express in our server 
-const express = require('express'); 
-const app = express(); 
+const express = require('express');
+const app = express();
 
 // Defining get request at '/' route 
-app.get('/', function(req, res) { 
-res.json({ 
-  message: 'Hello World',
-  data: {
-    name: 'John Doe',
-    age: 25
-  }
-}); 
-}); 
-
-// Defining get request at '/multiple' route 
-app.get('/pubg', function(req, res) { 
-res.json({ 
-	number: 1, 
-	name: 'John', 
-	gender: 'male'
-}); 
-}); 
+app.get('/', function (req, res) {
+  res.json(
+    {
+      status: "true",
+      msg: "success",
+      uuid: "02D434B0-A9FC-412D-8761-1390204F55DF",
+      device: "com.06F84074-A4B1-48A7-A705-1ADA3671D0F2",
+      type: "week",
+      start: "2023-09-29 04:59:21",
+      end: "2025-10-06 04:59:21"
+    }
+  );
 
 
+  // Defining get request at '/multiple' route 
+  app.get('/pubg', function (req, res) {
+    res.json({
+      number: 1,
+      name: 'John',
+      gender: 'male'
+    });
+  });
 
-// Setting the server to listen at port 3000 
-app.listen(3000, function(req, res) { 
-console.log("Server is running at port 3000"); 
-}); 
+
+
+  // Setting the server to listen at port 3000 
+  app.listen(3000, function (req, res) {
+    console.log("Server is running at port 3000");
+  }); 
